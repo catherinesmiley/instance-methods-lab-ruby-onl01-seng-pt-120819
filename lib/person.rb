5ba6ccc5ca9f51841c365ca113adb4b87ca93ed0
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-07 02:47:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,27 @@
-class Person 
-  def talk
-    puts "Hello World!"
-  end
+# class Person 
+#   def talk
+#     puts "Hello World!"
+#   end
   
-  def walk
-    puts "The Person is walking"
-  end
-end
+#   def walk
+#     puts "The Person is walking"
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
